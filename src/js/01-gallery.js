a81512a864cc92e95572947118ad708f953f0cc9
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-19 10:26:25 +0100

    task 1 simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,58 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-const basicLightbox = require('basiclightbox');
-
-console.log(basicLightbox);
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
-
-const modal = basicLightbox.create(
-  `<img class="modal-img" src="" width="800" height="600">`
-);
+// console.log(galleryItems);
 
 const getItemTemplate = ({ preview, description, original }) =>
-  `<div class="gallery__item" >
-        <a href="" class="gallery__link">
-            <img class="gallery__image" src ="${preview}" alt ="${description}" data-source="${original}">
-        </a>
-   </div>`;
+    `<a class="gallery__item"  href="${original}" >
+        <img class="gallery__image" src ="${preview}" alt ="${description}">
+     </a>`;
 
-const refs = {
-  gallery: document.querySelector('.gallery'),
-  modalImage: modal.element().querySelector('.modal-img'),
-  body: document.querySelector('body'),
-}
+const galleryRef = document.querySelector('.gallery');
 
 const renderGallery = () => {
   const lis = galleryItems.map((item) => getItemTemplate(item));
-  refs.gallery.insertAdjacentHTML('beforeend', lis.join(''));
+  galleryRef.insertAdjacentHTML('beforeend', lis.join(''));
 };
 
 renderGallery();
 
-const onClickShowModal = (event) => {
-  event.preventDefault();
-
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  refs.modalImage.src = event.target.dataset.source;
-
-  modal.show(() => refs.body.addEventListener('keydown', onEscPressCloseModal));
-}
-
-const onEscPressCloseModal = (event) => {
-  if (event.keyCode == 27) {
-    modal.close(() => refs.body.removeEventListener('keydown', onEscPressCloseModal));
-  }
-}
-
-const onClickCloseModal = (event) => {
-  event.preventDefault();
-  modal.close(() => refs.body.removeEventListener('keydown', onEscPressCloseModal));
-}
-
-refs.gallery.addEventListener('click', onClickShowModal);
-modal.element().addEventListener('click', onClickCloseModal);
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250ms', });
