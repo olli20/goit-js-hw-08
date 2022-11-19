2c48cdb6f8902422a8ce0e38be23ff851a983448
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-19 10:18:35 +0100

    some experiments with task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,58 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+const basicLightbox = require('basiclightbox');
+
+console.log(basicLightbox);
 // Change code below this line
 
 console.log(galleryItems);
+
+const modal = basicLightbox.create(
+  `<img class="modal-img" src="" width="800" height="600">`
+);
+
+const getItemTemplate = ({ preview, description, original }) =>
+  `<div class="gallery__item" >
+        <a href="" class="gallery__link">
+            <img class="gallery__image" src ="${preview}" alt ="${description}" data-source="${original}">
+        </a>
+   </div>`;
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  modalImage: modal.element().querySelector('.modal-img'),
+  body: document.querySelector('body'),
+}
+
+const renderGallery = () => {
+  const lis = galleryItems.map((item) => getItemTemplate(item));
+  refs.gallery.insertAdjacentHTML('beforeend', lis.join(''));
+};
+
+renderGallery();
+
+const onClickShowModal = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  refs.modalImage.src = event.target.dataset.source;
+
+  modal.show(() => refs.body.addEventListener('keydown', onEscPressCloseModal));
+}
+
+const onEscPressCloseModal = (event) => {
+  if (event.keyCode == 27) {
+    modal.close(() => refs.body.removeEventListener('keydown', onEscPressCloseModal));
+  }
+}
+
+const onClickCloseModal = (event) => {
+  event.preventDefault();
+  modal.close(() => refs.body.removeEventListener('keydown', onEscPressCloseModal));
+}
+
+refs.gallery.addEventListener('click', onClickShowModal);
+modal.element().addEventListener('click', onClickCloseModal);
---
+++ src/js/server.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4} = require('uuid');
+
+console.log(uuidv4());
+
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
