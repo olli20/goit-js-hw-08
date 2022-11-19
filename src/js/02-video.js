350e5abefb92d61362eb78cd27be5a8e320677d3
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-19 18:14:26 +0100

    task 2 part 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    
+const player = new Player(iframe);
+
+let currentTime = {};
+
+const onPlayTimeUpdate = (e) => {
+    // console.log("time1", e);
+    currentTime = e;
+    console.log(currentTime);
+};
+
+player.on('timeupdate', onPlayTimeUpdate);
+
+
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
