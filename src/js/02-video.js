d51e0287b7199d603bbe40c1b6404b81c89d32bd
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-21 10:15:52 +0100

    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,7 @@ console.log(throttle);
 const iframe = document.querySelector('iframe');    
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle((e) => { console.log(e) }, 1000));
+player.on('timeupdate', throttle((e) => { console.log(e.seconds) }, 1000));
 
 
 
