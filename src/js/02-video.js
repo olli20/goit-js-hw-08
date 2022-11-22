ac4645b891f175ec0bdda17636fdb4ca1bbb0f1b
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-22 14:37:31 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,6 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-console.log(throttle);
-
 const iframe = document.querySelector('iframe');    
 const player = new Player(iframe);
 
